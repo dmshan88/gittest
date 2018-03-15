1b2649d40e58abc8c8b265f70e7738683d27b49c
dmshan88 <dm_shan@163.com>
2018-03-15 22:45:07 +0800

    branch dev

--- app/Home/Controller/IndexController.class.php
+++ app/Home/Controller/IndexController.class.php
@@ -7,6 +7,6 @@ class IndexController extends Controller
 {
     public function index()
     {
-        echo "think 1.0 hhhh new machine simple aa";
+        echo "think 1.0 branch hhhh new machine simple aa";
     }
 }
